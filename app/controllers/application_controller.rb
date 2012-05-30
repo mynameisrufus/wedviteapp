5bd7590ed4e543728ce42c195a0eab0bd8af4f2c
Rufus Post <rufuspost@gmail.com>
2012-05-30 10:26:41 +1000

    Make markdown autolink use target _blank

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,18 @@ class ApplicationController < ActionController::Base
     render text: "User-Agent: *\nDisallow: /", layout: false
   end
 
+  class MarkdownRenderer < Redcarpet::Render::HTML
+    def link(link, title, alt_text)
+      "<a target=\"_blank\" href=\"#{link}\">#{alt_text}</a>"
+    end
+    def autolink(link, link_type)
+      "<a target=\"_blank\" href=\"#{link}\">#{link}</a>"
+    end
+  end
+
   def markdown
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+    @markdown ||= Redcarpet::Markdown.new(MarkdownRenderer,
       autolink: true, space_after_headers: true)
   end
 end
+
