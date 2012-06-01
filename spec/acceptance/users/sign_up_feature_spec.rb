fd03ba40f6313676f52543efc8fef4a0583a7525
Rufus Post <rufuspost@gmail.com>
2012-06-01 15:24:40 +1000

    added missing sign up feature

--- spec/acceptance/users/sign_up_feature_spec.rb
+++ spec/acceptance/users/sign_up_feature_spec.rb
@@ -1,13 +1,19 @@
 require 'acceptance/acceptance_helper'
 
 feature 'Sign up feature', %q{
-  In order to ...
-  As a ...
-  I want ...
+  In order to use wedvite
+  As a potential user
+  I want sign up
 } do
 
   scenario 'first scenario' do
-    true.should == true
+    visit '/'
+    click_link "Sign up for free"
+    fill_in "user[first_name]", with: "Motso"
+    fill_in "user[last_name]",  with: "Motta"
+    fill_in "user[email]",      with: "motso@matamata.com"
+    fill_in "user[password]",   with: "matamatarocks"
+    click_button "Sign up"
+    page.should have_content "Welcome! You have signed up successfully."
   end
-
 end
