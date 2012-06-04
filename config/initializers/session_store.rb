be060b13c4e1431d3dd9e15b489c02143b09062e
Rufus Post <rufuspost@gmail.com>
2012-06-04 16:56:16 +1000

    added subdomain cookie settings per env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
-WeddingInvitor::Application.config.session_store :cookie_store, key: '_wedding_invitor_session'
+WeddingInvitor::Application.config.session_store :cookie_store, key: 'wedvite_session', domain: {
+  production: '.wedviteapp.com',
+  staging: '.wedvite.net',
+  development: '.wedvite.dev',
+  test: :all
+}.fetch(Rails.env.to_sym)
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
