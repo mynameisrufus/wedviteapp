a685af2e0ff1f98776e863bdb912bf544ab8b78b
Rufus Post <rufuspost@gmail.com>
2012-06-07 13:07:21 +1000

    make file names unique for stationery

--- app/models/stationary_image.rb
+++ app/models/stationary_image.rb
@@ -3,6 +3,11 @@ class StationaryImage < ActiveRecord::Base
 
   validates_presence_of :stationary_id
 
+  validates :attachment_file_name, presence: true,  uniqueness: {
+    scope: :stationary_id,
+    message: "you have already uploaded that file"
+  }
+
   has_attached_file :attachment,
     storage: :s3,
     s3_credentials: "#{Rails.root}/config/s3.yml",
