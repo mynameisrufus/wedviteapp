7627ad0de7034912079bfd0132d4899fde791855
Rufus Post <rufuspost@gmail.com>
2012-06-21 14:39:52 +1000

    add in missing default mailer host so recover password works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,6 @@ WeddingInvitor::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'wedviteapp.com' }
 end
