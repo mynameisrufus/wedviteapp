65415b78c230297ddb8dd8b773a7a8096d8dc9c9
Rufus Post <rufuspost@gmail.com>
2012-07-14 17:55:37 +1000

    Remove cache as it breaks it.

--- app/controllers/site/pages_controller.rb
+++ app/controllers/site/pages_controller.rb
@@ -1,7 +1,4 @@
 class Site::PagesController < Site::BaseController
-
-  caches_page :home
-
   def home
     @lowest_possible_payment = Wedding::PRICE + Stationery.order('price ASC').pluck(:price).first
     @higest_possible_payment = Wedding::PRICE + Stationery.order('price DESC').pluck(:price).first
