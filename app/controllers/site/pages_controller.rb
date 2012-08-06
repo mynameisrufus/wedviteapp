a5b79d381cff27bf10e80ac61284b4ac8501f13d
Rufus Post <rufuspost@gmail.com>
2012-08-06 21:41:12 +1000

    Changed cache to 1 hour, dying inside...

--- app/controllers/site/pages_controller.rb
+++ app/controllers/site/pages_controller.rb
@@ -1,7 +1,7 @@
 class Site::PagesController < Site::BaseController
 
   caches_action :home, cache_path: 'site/pages/home',
-                       expires_in: 1.week
+                       expires_in: 1.hour
 
   def home
     @lowest_possible_payment = Wedding::PRICE + lowest_stationery_price
