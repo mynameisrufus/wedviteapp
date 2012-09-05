7ba5151a49febeb4b5f021d8a3ee2afa114a0e4f
Rufus Post <rufuspost@gmail.com>
2012-09-05 16:36:46 +1000

    Fixed markdown autolink for email links.

--- extras/wedding_invitor_markdown.rb
+++ extras/wedding_invitor_markdown.rb
@@ -3,8 +3,16 @@ class WeddingInvitorMarkdown
     def link(link, title, alt_text)
       "<a target=\"_blank\" href=\"#{link}\">#{alt_text}</a>"
     end
+
     def autolink(link, link_type)
-      "<a target=\"_blank\" href=\"#{link}\">#{link}</a>"
+      case link_type
+      when :email
+        "<a target=\"_blank\" href=\"mailto:#{link}\">#{link}</a>"
+      when :url
+        "<a target=\"_blank\" href=\"#{link}\">#{link}</a>"
+      else
+        "<a target=\"_blank\" href=\"#{link}\">#{link}</a>"
+      end
     end
   end
 
