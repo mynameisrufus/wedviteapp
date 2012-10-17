2f6a314ac62b0b9253ff5f34bad2e1ba6fc19495
Rufus Post <rufuspost@gmail.com>
2012-10-17 11:35:32 +1100

    Participants class and method added for use with email.

--- app/models/message.rb
+++ app/models/message.rb
@@ -14,6 +14,40 @@ class Message < ActiveRecord::Base
                 headline: "#{messageable.name} wrote:"
   end
 
+  class Participants
+    attr_reader :message, :users, :guests
+
+    def initialize message, guests = [], users = []
+      @message = message
+      @guests  = guests
+      @users   = users
+      collect
+    end
+
+    def collect
+      case @message.messageable_type
+      when 'User' then @users << @message.messageable
+      when 'Guest' then @guests << @message.messageable
+      end
+      @message.replies.each do |reply|
+        case reply.replyable_type
+        when 'User' then @users << reply.replyable
+        when 'Guest' then @guests << reply.replyable
+        end
+      end
+    end
+
+    def not messageable
+      @guests.delete messageable
+      @users.delete messageable
+      self
+    end
+  end
+
+  def participants
+    Participants.new self
+  end
+
   def by? type
     messageable_type == type.to_s.humanize
   end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user_reply) {
+    Reply.make replyable: User.make
+  }
+
+  let(:guest_reply) {
+    Reply.make replyable: Guest.make
+  }
+
+  before :each do
+    @message = Message.make messageable: User.make,
+                            replies: [user_reply, guest_reply]
+  end
+
+  it 'should return all the participants that are users' do
+    @message.participants.users.size.should eq 2
+  end
+
+  it 'should return all the participants that are guests' do
+    @message.participants.guests.size.should eq 1
+  end
+
+  it 'should remove with not method' do
+    @message.participants.not(@message.messageable).users.size.should eq 1
+  end
 end
