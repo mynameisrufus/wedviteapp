4f11946a3d5ac0ad8c0e2f7e9c5de9d14416c49b
Rufus Post <Rufus.Post@team.telstra.com>
2015-01-06 21:42:33 +1100

    Fix collab notify email

--- app/mailers/users/notify_collaborator_mailer.rb
+++ app/mailers/users/notify_collaborator_mailer.rb
@@ -3,7 +3,7 @@ class Users::NotifyCollaboratorMailer < Users::Mailer
     @sender  = options[:requestor]
     @user    = options[:user]
     @wedding = options[:wedding]
-    @url     = wedding_url(@wedding, subdomain: subdomain)
+    @url     = wedding_guestlist_url(@wedding, subdomain: subdomain)
 
     mail to: @user.email,
          subject: "You are now collaborating on #{@wedding.title}",
